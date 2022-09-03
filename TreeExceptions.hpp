3a9c8964a86d6aa147e6ed7532002dd39422b3ef
Sasyami <azarivan@rambler.ru>
2022-09-03 18:37:41 +0000

    Update TreeExceptions.hpp

--- TreeExceptions.hpp
+++ TreeExceptions.hpp
@@ -8,25 +8,43 @@
 #define WrongBypass 0
 #define NoSuchElement 1
 #define WrongInput 2
-
+#define DeleteRoot 3
+#define NoRotate 4
+#define NoRoot 5
 class TreeError: std::exception{
 private:
     static const char* MatrixErrorMsg (int msg){
 
         if (msg == WrongBypass){
 
-            auto string = "Bypass is wrong\n";
+            auto string = "Bypass is wrong";
             return string;
         }if (msg == NoSuchElement){
 
-            auto string = "No Such Element\n";
+            auto string = "No Such Element";
             return string;
         }
         if (msg == WrongInput){
 
-            auto string = "Wrong Input\n";
+            auto string = "Wrong Input";
             return string;
         }
+        if (msg == DeleteRoot){
+
+            auto string = "Tried to delete root";
+            return string;
+        }
+        if (msg == NoRotate){
+
+            auto string = "Cant rotate some root";
+            return string;
+        }
+        if (msg == NoRoot){
+
+            auto string = "No root in tree";
+            return string;
+        }
+
 
         auto string = "Something wrong, i can feel it(Maybe the error code?)\n";
         return string;
@@ -40,3 +58,4 @@ public:
     }
 };
 #endif //BINARYREEPROJECT_TREEEXCEPTIONS_HPP
+
